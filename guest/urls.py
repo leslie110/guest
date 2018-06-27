757ce48f1edd4d879c5bc84eef896c80207dc2a4
nishabi <616387521@qq.com>
2018-06-27 17:03:58 +0800

    开发结束

--- guest/urls.py
+++ guest/urls.py
@@ -15,17 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url,include
-from sign import views#导入应用模块
+from django.conf.urls import url, include
+from sign import views  # 导入应用模块
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r"^login/$",views.login),#添加index/路径配置
-    url(r"indexs/",views.indexs),
-    url(r"login_action",views.login_action),
-    url(r"event_manage/",views.event_manage),
-    url(r"^$",views.login),
-    url(r'accounts/login/$',views.login),
-    url(r"^search_name/$",views.search_name),
-    url(r"^guest_manage/",views.guest_manage),
+    url(r"^login/$", views.login),  # 添加index/路径配置
+    url(r"indexs/", views.indexs),
+    url(r"login_action", views.login_action),
+    url(r"event_manage/", views.event_manage),
+    url(r"^$", views.login),
+    url(r'accounts/login/$', views.login),
+    url(r"^search_name/$", views.search_name),
+    url(r"^guest_manage/", views.guest_manage),
+    url(r"^sign_index/(?P<eid>[0-9]+)/$", views.sign_index),
+    url(r"^sign_index_action/(?P<eid>[0-9]+)/$", views.sign_index_action),
+    url(r"logout/",views.logout)
+
 ]
--- sign/views.py
+++ sign/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from sign.models import Event,Guest
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 #登录首页
@@ -76,3 +77,41 @@ def guest_manage(request):
     return render(request,"guest_manage.html",{"user":username,
                                                "guests":contacts
     })
+
+#签到页面
+@login_required()
+def sign_index(request,eid):
+    event = get_object_or_404(Event,id=eid)
+    return render(request,'sign_index.html',{'event':event})
+
+#签到动作
+@login_required()
+def sign_index_action(request,eid):
+    event = get_object_or_404(Event, id=eid)
+    phone = request.POST.get('phone','')
+    print (phone)
+
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,
+                                                 'hint':'phone error.'})
+    result = Guest.objects.filter(phone=phone,event_id=eid)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,
+                                                 'hint':'event id or phone error.'})
+    result = Guest.objects.get(phone=phone, event_id=eid)
+    if result.sign:
+        return render(request, 'sign_index.html', {'event': event,
+                                                   'hint': 'user has sign in.'})
+    else:
+        Guest.objects.filter(phone=phone,event_id=eid).update(sign=1)
+        return render(request, 'sign_index.html', {'event': event,
+                                                   'hint': 'sign in success!',
+
+                                                   'guest':result})
+#退出登录
+@login_required()
+def logout(request):
+    auth.logout(request)  #退出登录
+    response = HttpResponseRedirect('/')
+    return response
